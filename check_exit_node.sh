3b6ee65022b95d9d81929c3f1fe805299c3342b0
bret99 <64787351+bret99@users.noreply.github.com>
2020-05-04 18:53:04 +0000

    Update check_exit_node.sh

--- check_exit_node.sh
+++ check_exit_node.sh
@@ -8,9 +8,9 @@ function notify {
 
 function ip {
         flagToFind=`whois $(curl ifconfig.me)|grep country|sed 's/country:'//|sort --unique|sed -e 's/\(.*\)/\L\1/'`
-        flagToShow=`cd /usr/share/flags/countries/16x11/;ls|grep $flagToFind`
+        flagToShow=`cd /usr/share/iso-flags-svg/country-4x3/;ls|grep $flagToFind`
 	MYIP=`whois $(curl ifconfig.me)|grep -E -i "cidr|country|city|address|organization|descr"|sort`
-        notify "$MYIP\npuplic IP:      $(curl ifconfig.me)"
+        notify "$MYIP\npuplic IP:\t$(curl ifconfig.me)"
         sleep 1.0
-        notify-send "is your exit node " -i /usr/share/flags/countries/16x11/$flagToShow
+        notify-send "is your exit node " -i /usr/share/iso-flags-svg/country-4x3/$flagToShow
 }
