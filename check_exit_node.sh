1650d535b2c78915f9e7171e036e070fab3b69a7
bret99 <64787351+bret99@users.noreply.github.com>
2020-05-04 13:51:48 +0000

    Create check_exit_node.sh

---
+++ check_exit_node.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function notify {
+	if [ -e /usr/bin/notify-send ]; then
+		/usr/bin/notify-send "AnonSurf" "$1"
+	fi
+}
+
+function ip {
+        flagToFind=`whois $(curl ifconfig.me)|grep country|sed 's/country:'//|sort --unique|sed -e 's/\(.*\)/\L\1/'`
+        flagToShow=`cd /usr/share/flags/countries/16x11/;ls|grep $flagToFind`
+	MYIP=`whois $(curl ifconfig.me)|grep -E -i "cidr|country|city|address|organization|descr"|sort`
+        notify "$MYIP\npuplic IP:      $(curl ifconfig.me)"
+        sleep 1.0
+        notify-send "is your exit node " -i /usr/share/flags/countries/16x11/$flagToShow
+}
