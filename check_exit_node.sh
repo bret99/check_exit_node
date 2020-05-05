ff2b40b7cde914554be58c70c3355d9750068e3e
bret99 <64787351+bret99@users.noreply.github.com>
2020-05-05 10:13:15 +0000

    Update check_exit_node.sh

--- check_exit_node.sh
+++ check_exit_node.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 function notify {
-	if [ -e /usr/bin/notify-send ]; then
-		/usr/bin/notify-send "Your public IP:" "$1"
-	fi
+    if [ -e /usr/bin/notify-send ]; then
+        /usr/bin/notify-send "Your public IP:" "$1"
+    fi
 }
 
-function ip {
-        flagToFind=`whois $(curl ifconfig.me)|grep country|sed 's/country:'//|sort --unique|sed -e 's/\(.*\)/\L\1/'`
-        flagToShow=`cd /usr/share/iso-flags-svg/country-4x3/;ls|grep $flagToFind`
-	MYIP=`whois $(curl ifconfig.me)|grep -E -i "cidr|country|city|address|organization|descr"|sort`
-        notify "$MYIP\npuplic IP:\t$(curl ifconfig.me)"
-        sleep 1.0
-        notify-send "is your exit node " -i /usr/share/iso-flags-svg/country-4x3/$flagToShow
-}
+IP=$(curl ifconfig.me)
+countryToFind=`whois $IP|grep country|sed 's/country://; s/Country://'|sort --unique`                                                          
+flagToFind=`echo $countryToFind|sed -e 's/\(.*\)/\L\1/'`   
+flagToShow=`cd /usr/share/iso-flags-svg/country-4x3/;ls|grep $flagToFind`
+MYIP=`whois $IP|grep -E -i "cidr|country|city|address|organization|descr"|sort`
+notify "$MYIP\npuplic IP:\t$IP"
+sleep 1.0
+notify-send "$countryToFind  is your exit node " -i /usr/share/iso-flags-svg/country-4x3/$flagToShow
