509411f4391661552fe8c43e4ee2c0f40ebf1399
bret99 <akloy2@protonmail.com>
2020-08-19 16:06:51 +0400

    Just read

--- anonsurf_enhanced.sh
+++ anonsurf_enhanced.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-# PUT THIS SCRIPT INSIDE /usr/bin/ AS "USER INTERFACEs"
 # anonsurf is free software: you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
 # published by the Free Software Foundation, either version 3 of the
